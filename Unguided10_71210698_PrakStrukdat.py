4b7ae87815e46fb260a7f41c09cf53ab93a221ca
AriTriani <93314786+AriTriani@users.noreply.github.com>
2023-05-08 15:54:02 +0700

    Update Unguided10_71210698_PrakStrukdat.py

--- Unguided10_71210698_PrakStrukdat.py
+++ Unguided10_71210698_PrakStrukdat.py
@@ -1,20 +1,20 @@
-def mergesort(data):
+def merge_sort_ascending(data):
     if len(data) > 1:
         mid = len(data) // 2
         left = data[:mid]
         right = data[mid:]
 
-        mergesort(left)
-        mergesort(right)
+        merge_sort_ascending(left)
+        merge_sort_ascending(right)
 
         i = 0
         j = 0
         k = 0
-        
+
         while i < len(right) and j < len(left):
-            if right[i] >= left[j] :
-              data[k] = right[i]
-              i += 1
+            if right[i] < left[j]:
+                data[k] = right[i]
+                i += 1
             else:
                 data[k] = left[j]
                 j += 1
@@ -26,14 +26,16 @@ def mergesort(data):
             k += 1
 
         while j < len(left):
-            data[k]=left[j]
+            data[k] = left[j]
             j += 1
             k += 1
-        return data
+
+    return data
 
 lst1 = [1,2,3,4,5,6,7,8,9,10,19,24,12,6,129,59,1,2000,3,56]
 lst2 = [20,21,22,23,24,25,26,27]
 lst3 = [30,29,31,33,19,20,31,21,59]
-print(mergesort(lst1))
-print(mergesort(lst2))
-print(mergesort(lst3))
+
+print(merge_sort_ascending(lst1))
+print(merge_sort_ascending(lst2))
+print(merge_sort_ascending(lst3))
